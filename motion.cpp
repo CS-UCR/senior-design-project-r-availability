2fd55a5454a257954d17c6fd6a635d60e05e8070
ruiz-jorge <71472801+ruiz-jorge@users.noreply.github.com>
2021-11-21 14:19:42 -0800

    Adding motion sensor states to track occupants

--- motion.cpp
+++ motion.cpp
@@ -18,7 +18,38 @@ int total_chairs;
 int occupants;
 task task1, task2;
 int P_tick(int state){//function to change room count
-	
+	switch (state) { // transitions
+		case PSM_wait:
+			if (digitalRead(leftSensor) == broken)
+				state = out1;
+			else if (digitalRead(rightSensor) == broken)
+				state = in1;
+			break;
+		case out1:
+			if (digitalRead(leftSensor) == broken && digitalRead(rightSensor) == broken)
+				state = out2;
+			else if (digitalRead(leftSensor) == connected && digitalRead(rightSensor) == connected)
+				state = PSM_wait;
+			break;
+		case out2:
+			if (digitalRead(leftSensor) == connected && digitalRead(rightSensor) == connected) {
+				state = PSM_wait;
+				occupants--;
+			}
+			break;
+		case in1:
+			if (digitalRead(rightSensor) == broken && digitalRead(leftSensor) == broken)
+				state = in2;
+			else if (digitalRead(rightSensor) == connected && digitalRead(leftSensor) == connected)
+				state = PSM_wait;
+			break;
+		case in2:
+			if (digitalRead(rightSensor) == connected && digitalRead(leftSensor) == connected) {
+				state = PSM_wait;
+				occupants++;
+			}
+			break;
+	}
 }
 int C_tick(int state){//function to make api call to mongodb
 
