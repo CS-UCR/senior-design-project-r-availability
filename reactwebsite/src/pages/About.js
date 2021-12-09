d67117b3689c66ad9376db9b4981a6f5b2ca8e4b
XiaoFan-UCR-CompEngr <72422851+XiaoFan-UCR-CompEngr@users.noreply.github.com>
2021-12-08 18:33:06 -0800

    Add files via upload

--- reactwebsite/src/pages/About.js
+++ reactwebsite/src/pages/About.js
@@ -1,13 +1,26 @@
+import { Container } from '@material-ui/core';
 import React from 'react';
 import Navbar from '../components/Navbar';
 import './About.css';
 
+
+const handleClick = () =>
+{
+console.log('hello, highlanders, Lets check out where you want to go');
+}
+
+
+
 function About() {
   return (
     <>
+       <div className="about">
+      <h1> R'MAP</h1>
       <Navbar />
-      <div>
-      Explaination about the project coming soon
+
+      This page shows the map of UCR.
+      <img src="UCRMAP3.jpeg" alt="" ></img>
+      <button onClick={handleClick}>click me</button>
       </div>
     </> 
   )
