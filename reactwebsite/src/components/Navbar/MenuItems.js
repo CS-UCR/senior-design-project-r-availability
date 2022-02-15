44aadbf056d4dc182d182439eb88c827fdc6fc84
shubham-batra <shubham.batra@outlook.com>
2022-02-15 03:42:19 -0800

    deleted unnecessary node_modules

--- reactwebsite/src/components/Navbar/MenuItems.js
+++ reactwebsite/src/components/Navbar/MenuItems.js
@@ -10,11 +10,6 @@ export const MenuItems = [
         cName: 'nav-links'
     },
     {
-        title: 'Records',
-        url: '/recordList',
-        cName: 'nav-links'
-    },
-    {
         title: 'Log Out',
         url: '/',
         cName: 'nav-links'
