57cedc4629052e99dd70297057eb9fe76eeba11b
shubham-batra <shubham.batra@outlook.com>
2022-02-21 17:01:36 -0800

    changed script

--- reactwebsite/src/components/LoginButton.js
+++ reactwebsite/src/components/LoginButton.js
@@ -1,8 +1,6 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import './LoginButton.css';
 
-// When working locally, use "http://localhost:5000/home" instead of 
-
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 // When running locally, input 'http://localhost:3000/home'as the redirectURI, otherwise, should be 'https://r-availability.herokuapp.com/home'
