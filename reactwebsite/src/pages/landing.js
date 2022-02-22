3da9ef7318ac205792186cc7e704680236aae9ba
shubham-batra <shubham.batra@outlook.com>
2022-02-21 22:52:02 -0800

    changes to UI mostly

--- reactwebsite/src/pages/landing.js
+++ reactwebsite/src/pages/landing.js
@@ -3,15 +3,42 @@ import {Link } from "react-router-dom";
 import LoginButton from '../components/LoginButton';
 import Profile from '../components/Profile';
 import {withAuthenticationRequired} from "@auth0/auth0-react";
+import './landing.css';
 
 
   function Landing() {
     return (
           <div>
-              <h1><center>This is the R'Availability Landing Page</center></h1>
+            <div class = "title">
+              <h1> R'Availability </h1>
+              </div>
+              <br></br>
+              <div class="flex-container">
+                <div class="box1">
+                    A tool for all UCR students and Faculty
+                </div>
+                <div class = "box2"> 
+                  <h2>
+                    <div class = "UCRimage">
+                  <img src="newUCR.jpg" alt="UCR"/>
+                    </div>
+                    </h2>
+                    </div>
+              </div>
+              <div class = "flex-container2">
+                <div class="box3">
+                  Image 1
+                </div>
+                <div class="box4">
+                  Image 2
+                </div>
+                <div class="box5">
+                  Image 3
+                </div>
+              </div>
               <center><LoginButton /></center>
-              <Profile />
           </div>
+          
     );
   }
     export default Landing;
