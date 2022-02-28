7b1661ec46031b4745364976ba699354f7f7e470
tgustafson2 <gus.duckfan@gmail.com>
2022-02-28 14:03:22 -0800

    Cleaned up program somewhat

--- motion.cpp
+++ motion.cpp
@@ -24,7 +24,7 @@ struct task{
 	signed char state;
 	unsigned long period;
 	unsigned elapsedTime;
-	int (*TickFct)(int);
+	int (*TickFct)(int,int &, int &);
 };
 enum P_States{PSM_wait, out1, out2, out3, in1, in2, in3};
 enum C_States{CSM_wait, send};
@@ -32,21 +32,21 @@ int connected=1;
 int broken=0;
 int leftSensor=24;
 int rightSensor=27;
-int total_chairs;
-int occupants;
+//int total_chairs;
+//int occupants;
 mongocxx::instance inst{};
 const auto uri = mongocxx::uri{"mongodb+srv://ShubhamBatra:M%40gic2001@ravailability-database.dofjv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"};
 mongocxx::client conn {uri};
 mongocxx::database db = conn["myFirstDatabase"];
-mongocxx::collection coll = db["records"];
+mongocxx::collection coll = db["TTP"];
 task task1, task2;
-void reset(){occupants=0;}
-void update(int i){
+void reset(int &occupants){occupants=0;}
+void update(int i, int &occupants){
 	if((i>0&&occupants<25)||(i<0&&occupants>1)){
 	occupants+=i;}
 }
 
-int P_tick(int state){//function to change room count
+int P_tick(int state, int &occupants, int &total_chairs){//function to change room count
 	switch (state) { // transitions
 		case PSM_wait:
 			if (digitalRead(leftSensor) == broken)
@@ -63,7 +63,7 @@ int P_tick(int state){//function to change room count
 		case out2:
 			if (digitalRead(leftSensor) == connected && digitalRead(rightSensor) == connected) {
 				state = PSM_wait;
-				update(-1);
+				update(-1,occupants);
 			}
 			break;
 		case in1:
@@ -75,13 +75,13 @@ int P_tick(int state){//function to change room count
 		case in2:
 			if (digitalRead(rightSensor) == connected && digitalRead(leftSensor) == connected) {
 				state = PSM_wait;
-				update(1);
+				update(1,occupants);
 			}
 			break;
 	}
 	return state;
 }
-int C_tick(int state){//function to make api call to mongodb
+int C_tick(int state,int &occupants,int &total_chairs){//function to make api call to mongodb
 	cout<<"Uploading to the database\n";
 	time_t current=time(0);
 	time(&current);
@@ -102,8 +102,8 @@ int main(){
 	wiringPiSetup();
 	pinMode(leftSensor,INPUT);
 	pinMode(rightSensor,INPUT);
-	total_chairs=25;
-	occupants=0;
+	int total_chairs=25;
+	int occupants=0;
 	bool track=true;
 	time_t current=time(0);
 	tm *ltm=localtime(&current);
@@ -123,7 +123,7 @@ int main(){
 		int temp=occupants;
 		for(int i=0;i<numtasks;i++){
 			if(tasks[i]->elapsedTime==tasks[i]->period&i!=1){
-				tasks[i]->state=tasks[i]->TickFct(tasks[i]->state);
+				tasks[i]->state=tasks[i]->TickFct(tasks[i]->state, occupants, total_chairs);
 				tasks[i]->elapsedTime=0;
 			}
 			if(i!=1)
@@ -138,14 +138,14 @@ int main(){
 					//cout<<"checking if start of minute\n";
 				if(tasks[i]->elapsedTime==1000){
 				tasks[i]->elapsedTime=0;
-				tasks[i]->state=tasks[i]->TickFct(tasks[i]->state);}
+				tasks[i]->state=tasks[i]->TickFct(tasks[i]->state,occupants,total_chairs);}
 				//else tasks[i]->elapsedTime=1000;
 				}
 				else tasks[i]->elapsedTime=1000;
 				}
 			}
 			cur=ltm->tm_hour;
-			if(cur==0)reset();
+			if(cur==0)reset(occupants);
 		}
 		if(occupants!=temp)cout<<"There are "<<occupants<<" occupants\n"<<"There are "<<total_chairs<<" chairs\n";
 		delay(1);
