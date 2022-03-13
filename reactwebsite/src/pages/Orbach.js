213f4b3d0ee2ba614b70ba6d348cff4b4e3cc695
shubham-batra <shubham.batra@outlook.com>
2022-03-12 20:27:26 -0800

    Xiao room changes made

--- reactwebsite/src/pages/Orbach.js
+++ reactwebsite/src/pages/Orbach.js
@@ -1,11 +1,18 @@
 import {useState, useEffect} from "react";
 import Axios from "axios" 
 import Navbar from '../components/Navbar/Navbar.js';
-
-// Locally, use "http://localhost:5000/getOrbach" instead of "https://r-availability.herokuapp.com/getOrbach"
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 
 function Orbach() {
   const [listOfUsers, setListOfUsers] = useState([]);
+
+  // Locally, use "http://localhost:5000/getOrbach" instead of "https://r-availability.herokuapp.com/getOrbach"
+
 
   useEffect(() => {
       Axios.get("https://r-availability.herokuapp.com/getOrbach").then((response) =>  {
@@ -15,6 +22,7 @@ function Orbach() {
       } )
   }, []);
 
+
   return (
     <div className="App">
       <Navbar />
@@ -22,18 +30,35 @@ function Orbach() {
           {listOfUsers.map((Orbach) => {
             return (
               <div> 
-                <h1>Current Orbach Info - Updated every 15 minutes</h1>
-                <br></br>
-                <h1>Occupancy: {Orbach.occupancy} </h1>
+
+<Card sx={{ maxWidth: 1300 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="250"
+          image= '/assets/orbach.jpeg'
+          alt="Orbach library"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+          Current Orbach Info - Updated every 15 minutes
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            
+          <h1>Occupancy: {Orbach.occupancy} </h1>
                 <h1>Seats: {Orbach.seats} </h1>
                 <h1>Time: {Orbach.time} </h1>
-                <br></br>
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+              
                 <script type = "text/JavaScript">
                   function AutoRefresh(t) {
-                    setTimeout("location.reload(true);", 5000)
+                    setTimeout("location.reload(true);", 15000)
                   }
               </script>
-              <body onload = "JavaScript:AutoRefresh(5000);"></body>
+              <body onload = "JavaScript:AutoRefresh(15000);"></body>
               </div>
             );
           })}
--- reactwebsite/src/pages/TTP.js
+++ reactwebsite/src/pages/TTP.js
@@ -1,11 +1,16 @@
 import {useState, useEffect} from "react";
 import Axios from "axios" 
 import Navbar from '../components/Navbar/Navbar.js';
-
-// Locally, use "http://localhost:5000/getTTP" instead of "https://r-availability.herokuapp.com/getTTP"
-
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 function TTP() {
   const [listOfUsers, setListOfUsers] = useState([]);
+
+  // Locally, use "http://localhost:5000/getTTP" instead of "https://r-availability.herokuapp.com/getTTP"
 
   useEffect(() => {
       Axios.get("https://r-availability.herokuapp.com/getTTP").then((response) =>  {
@@ -22,18 +27,35 @@ function TTP() {
           {listOfUsers.map((TTP) => {
             return (
               <div> 
-                <h1>Current TTP Info - Updated every 15 minutes</h1>
-                <br></br>
-                <h1>Occupancy: {TTP.occupancy}</h1>
+
+
+<Card sx={{ maxWidth: 1300 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="250"
+          image= '/assets/TTP.jpg'
+          alt="TTP"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+          Current TTP Info - Updated every 15 minutes
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+
+                <h1>Occupancy: {TTP.occupancy} </h1>
                 <h1>Seats: {TTP.seats} </h1>
                 <h1>Time: {TTP.time} </h1>
-                <br></br>
+                </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
                 <script type = "text/JavaScript">
                   function AutoRefresh(t) {
-                    setTimeout("location.reload(true);", 5000)
+                    setTimeout("location.reload(true);", 15000)
                   }
               </script>
-              <body onload = "JavaScript:AutoRefresh(5000);"></body>
+              <body onload = "JavaScript:AutoRefresh(15000);"></body>
               </div>
             );
           })}
